4f3520c0ee69adde15d79b53c67a664e07d5e76c
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-07-24 23:41:27 +0100

    texting gContextMenuTarget

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 var gServerUrl = "https://explaain-api-develop.herokuapp.com";
 if (window.location.hostname == "explaain-cms.herokuapp.com")
   gServerUrl = "https://explaain-api.herokuapp.com";
-    
+
 var gApiKey = readCookie("apiKey");
 var gContextMenuTarget = null;
 var schemas = {};
@@ -23,9 +23,9 @@ var sendMessageToPreviewFrame = function(id, action) {
 };
 
 $(function() {
-  
+
   $("#server-url").text("Connected to "+gServerUrl);
-  
+
   if (!gApiKey)
     promptForApiKey();
 
@@ -40,13 +40,13 @@ $(function() {
       $.ajax({
         url: gServerUrl+"/"+schemaName,
       }).done(function(jsonSchema) {
-        
+
         // If we have a template for this schema add it to the New Card dropdown
         if ($('script[data-template="'+schemaName+'"]').length != 0) {
           $("#newCardBtn .dropdown-menu").append('<li><a href="#" onclick="showCard(null, \''+schemaName+'\')">'+schemaName+'</a></li>');
           $("#newCardContextMenu").append('<li><a href="#" onclick="hideContextMenu(); showCard(null, \''+schemaName+'\', true)"><i class="fa fa-fw fa-plus-circle"></i> Add '+schemaName+'</a></li>');
         }
-        
+
         schemas[schemaName] = jsonSchema;
       });
     });
@@ -56,11 +56,11 @@ $(function() {
     if (event.data.action = "edit")
       return showCard(event.data.id);
   }, false);
-  
+
   // Intercept the search box
   $("#search").submit(function(e) {
     e.preventDefault();
-    
+
     $("#searchResults").html('');
 
     var searchesReturned = 0;
@@ -83,7 +83,7 @@ $(function() {
     };
     return false;
   });
-  
+
   // Intercept the *context menu* search box
   $('#contextmenu #addLinkContextMenu input[name="search"]').keyup(function(e) {
     if (e.keyCode == 13) {
@@ -121,11 +121,11 @@ $(function() {
     });
     return o;
   };
-  
+
   // Handle focusing cards when child element (e.g. input field) has focus
   var handle = ally.style.focusWithin();
 
-  $(document).click(function(event) { 
+  $(document).click(function(event) {
     // Clicking anywhere outside of the context menu when it is visible
     // should hide it.
     if (!$(event.target).closest('#contextmenu').length && $('#contextmenu').is(":visible")) {
@@ -134,13 +134,13 @@ $(function() {
       $('#contextmenu').hide();
     }
   })
-  
+
   $("#newCardContextMenuBtn").bind('click touch', function(e) {
     $("#newCardContextMenuBtn .dropdown-menu").toggle();
   });
-  
+
   // Show card on load when running a local test environment
-  if (window.location.hostname == "localhost") 
+  if (window.location.hostname == "localhost")
    showCard("https://explaain-api-develop.herokuapp.com/Person/57690520ad606e1100ca163e");
 });
 
@@ -166,12 +166,12 @@ function showCard(uri, schemaName, linkToSelectContextMenuTarget) {
     // on elements that can otherwise cause the card to disappear 'oddly'.
     // Hiding and re-showing a card has the benifit that cards remember their
     // last positions. A downside is a cards contents could be stale, but that
-    // can be easily addressed in a future update - and is something we'd 
+    // can be easily addressed in a future update - and is something we'd
     // want to handle anyway with server side events in case a card is updated
     // by someone else on another browser!
     beforeClose: function(event, ui) {
       $(this).parents(".ui-dialog").css({visibility: 'hidden', opacity: 0, zIndex: 0});
-      
+
       // Move focus to card with highest zIndex
       var highestZIndex = 0;
       var cardWithHighestZIndex = null;
@@ -184,14 +184,14 @@ function showCard(uri, schemaName, linkToSelectContextMenuTarget) {
       });
       if (cardWithHighestZIndex)
         $(cardWithHighestZIndex).focus();
-      
+
       event.preventDefault();
       return false;
     }
   };
-    
+
   if (uri) {
-    // Display existing card 
+    // Display existing card
 
     if (!schemaName)
       schemaName = uri.split("/")[uri.split("/").length-2];
@@ -199,7 +199,7 @@ function showCard(uri, schemaName, linkToSelectContextMenuTarget) {
     // If card with that URI is already displayed, bubble it up to the top
     if ($('#cards .card[data-id="'+uri+'"]').length > 0) {
       // Loop through all visible cards and get the current highest zIndex value
-      // Starting with z-Index of at least 10 to make sure it displays above 
+      // Starting with z-Index of at least 10 to make sure it displays above
       // other elements in the same area, such as search results.
       var highestZIndex = 10;
       $("#cards .ui-dialog").each(function() {
@@ -207,7 +207,7 @@ function showCard(uri, schemaName, linkToSelectContextMenuTarget) {
         if (zIndex > highestZIndex)
           highestZIndex = zIndex;
       });
-      
+
       // Check if card is visible or not
       //if ($('#cards .card[data-id="'+uri+'"]').parents(".ui-dialog").css('visibility') == 'hidden') {
       //}
@@ -217,44 +217,44 @@ function showCard(uri, schemaName, linkToSelectContextMenuTarget) {
       }).done(function(entity) {
         // Load template for card based on schema type
         var template = $.templates('script[data-template="'+schemaName+'"]');
-    
+
         entity.id = entity['@id'];
         entity.links = (entity.links) ? entity.links.join(',') : [];
         entity.description = marked(entity.description);
-        
+
         // Populate template
         var html = template.render(entity);
-        
+
         // Inject HTML into container
         $('#cards .card[data-id="'+uri+'"]').html(html);
 
         // Reinitalize textara
         initaliseTextarea($('.textarea', $('#cards .card[data-id="'+uri+'"]')));
-        
+
         $('.textarea a[href]:not("\\#")', card).popover({
            placement: 'right',
            trigger: 'hover',
            html: true,
            content: '…'
          });
-         
+
         // Display card
         $('#cards .card[data-id="'+uri+'"]').parents(".ui-dialog").css({zIndex: highestZIndex+1, visibility: 'visible', opacity: 1}).focus();
       });
 
-      return; 
+      return;
     }
 
     // Check we have a template for this schema type
     if ($('script[data-template="'+schemaName+'"]').length == 0)
       return console.log("Display Card Error: No template defined for "+schemaName+" schema");
-    
+
     $.ajax({
       url: uri
     }).done(function(entity) {
 
       cardDialogOptions.title = schemaName+" Card";
-    
+
       // Load template for card based on schema type
       var template = $.templates('script[data-template="'+schemaName+'"]');
 
@@ -264,14 +264,14 @@ function showCard(uri, schemaName, linkToSelectContextMenuTarget) {
 
       // Populate template
       var html = template.render(entity);
-  
+
       // Create new container for card (inside parent) and inject html
       var card = $('<div data-id="'+entity['@id']+'" data-schema="'+schemaName+'" class="card">'+html+'</div>');
 
       card.dialog(cardDialogOptions);
-      
+
       initaliseTextarea($('.textarea', card));
-      
+
       $('.textarea a[href]:not("\\#")', card).popover({
          placement: 'right',
          trigger: 'hover',
@@ -282,36 +282,36 @@ function showCard(uri, schemaName, linkToSelectContextMenuTarget) {
     });
   } else {
     // Display new, blank card
-    
+
     // Check we have a template for this schema type
     if ($('script[data-template="'+schemaName+'"]').length == 0)
       return console.log("Display New Card Error: No template defined for "+schemaName+" schema");
-  
+
     // Load template for card based on schema type
     var template = $.templates('script[data-template="'+schemaName+'"]');
-    
+
     // Populate template
     var html = template.render({
       id: "No ID assigned yet",
       name: gContextMenuTarget.innerText || ""
     });
-  
+
     // Create new container for card (inside parent) and inject html
     var card = $('<div data-schema="'+schemaName+'" class="card">'+html+'</div>');
 
     cardDialogOptions.title = schemaName+" Card";
     card.dialog(cardDialogOptions);
     initaliseTextarea($('.textarea', card));
-    
+
     $('.textarea a[href]:not("\\#")', card).popover({
        placement: 'right',
        trigger: 'hover',
        html: true,
        content: '…'
      });
-     
+
     if (linkToSelectContextMenuTarget === true) {
-      // @FIXME Super hacky, but waits before saving card otherwise it might 
+      // @FIXME Super hacky, but waits before saving card otherwise it might
       // not have been rendered yet. There isn't a call back we can use :-(
       // but we should really test the DOM to see if the card is actually ready.
       setTimeout(function() {
@@ -326,20 +326,20 @@ function showCard(uri, schemaName, linkToSelectContextMenuTarget) {
 function saveCard(card, callback) {
   var schemaName =  $(card).parents(".card").attr('data-schema');
   var uri =  $(card).parents(".card").attr('data-id');
-  
+
   if (uri) {
     // Update existing card
     var formData = $(card).parents('form').serializeObject();
-    
+
     formData.description = $('.textarea[-data-name="description"]', $(card).parents('form')).html();
     formData.description = toMarkdown(formData.description);
-    
+
     formData.links = [];
     $("a", $('.textarea[-data-name="description"]', $(card).parents('form'))).each(function(){
       if (formData.links.indexOf(this.href) == -1)
         formData.links.push(this.href);
     });
-    
+
     $.ajax({
       type: 'PUT',
       url: uri,
@@ -365,16 +365,16 @@ function saveCard(card, callback) {
   } else {
     // Create new card
     var formData = $(card).parents('form').serializeObject();
-    
+
     formData.description = $('.textarea[-data-name="description"]', $(card).parents('form')).html();
     formData.description = toMarkdown(formData.description);
-    
+
     formData.links = [];
     $("a", $('.textarea[-data-name="description"]', $(card).parents('form'))).each(function(){
       if (formData.links.indexOf(this.href) == -1)
         formData.links.push(this.href);
     });
-    
+
     $.ajax({
       type: 'POST',
       url: gServerUrl+"/"+schemaName,
@@ -386,7 +386,7 @@ function saveCard(card, callback) {
     .done(function(entity) {
       $(card).parents(".card").attr('data-id', entity['@id']);
       $('input[name="id"]', $(card)).val(entity['@id']);
-      
+
       showCard(entity['@id']);
 
       sendMessageToPreviewFrame(entity['@id'], 'create');
@@ -407,10 +407,10 @@ function saveCard(card, callback) {
 function deleteCard(card) {
   if (confirm("Are you sure you want to delete this card?\n\nThis action cannot be undone.")) {
     var uri =  $(card).parents(".card").attr('data-id');
-    
+
     if (!uri)
       return $(card).parents(".ui-dialog").css({visibility: 'hidden', opacity: 0, zIndex: 0});
-    
+
     $.ajax({
       type: 'DELETE',
       url: uri,
@@ -425,7 +425,7 @@ function deleteCard(card) {
       // hiding elements for now.
       $(card).parents(".ui-dialog").css({visibility: 'hidden', opacity: 0, zIndex: 0});
       updateView(uri);
-      
+
       // Move focus to card with highest zIndex
       var highestZIndex = 0;
       var cardWithHighestZIndex = null;
@@ -438,7 +438,7 @@ function deleteCard(card) {
       });
       if (cardWithHighestZIndex)
         $(cardWithHighestZIndex).focus();
-      
+
       toast("Card deleted", "success");
     })
     .fail(function(err) {
@@ -487,7 +487,7 @@ function updateView(id) {
 }
 
 function initaliseTextarea(textarea) {
-  
+
   textarea.toTextarea({
     allowHTML: false,//allow HTML formatting with CTRL+b, CTRL+i, etc.
     allowImg: false,//allow drag and drop images
@@ -495,7 +495,7 @@ function initaliseTextarea(textarea) {
     pastePlainText: true,//paste text without styling as source
     placeholder: false//a placeholder when no text is entered. This can also be set by a placeholder="..." or data-placeholder="..." attribute
   });
-  
+
 
   // Detect if marked block in textarea was clicked
   textarea.bind('click touch', function(e){
@@ -515,7 +515,7 @@ function initaliseTextarea(textarea) {
     // Do nothing if no text selected
     if (selectedText.length === 0)
       return;
-    
+
     var response;
     var el = document.elementFromPoint(e.clientX, e.clientY);
     if (el.tagName == "A") {
@@ -529,12 +529,12 @@ function initaliseTextarea(textarea) {
       a.href = "#";
       a.innerText = selectedText;
       range.insertNode(a);
-      
+
       showContextMenu('addLinkContextMenu', a);
       return;
     }
   });
-  
+
 }
 
 function htmlEncode(value){
@@ -592,33 +592,34 @@ function rangeSelectsSingleNode(range) {
 }
 
 function showContextMenu(contextMenuId, target) {
-  
+
   // If no target but there is an old target, use the last target
   if (!target && target !== false && gContextMenuTarget)
     target = gContextMenuTarget;
-  
+
   // If there is still no target then return
   // @TODO Display at current mouse location
   if (!target)
     return;
-  
+
   var viewportOffset = target.getBoundingClientRect();
   x = viewportOffset.left;
   y = viewportOffset.bottom;
-  
+
   // Hide + reset menu
   $("#contextmen").hide();
   $("#contextmenu ul").hide();
   $('#contextmenu #addLinkContextMenu input[type="text"]').val(target.innerText);
   $("#contextmenu #addLinkContextMenu li.searchResult").remove();
-  
+
   gContextMenuTarget = target;
-  
+  console.log(gContextMenuTarget);
+
   // Show menu
   $("#contextmenu #"+contextMenuId).show();
   $("#contextmenu").css({left: x, top: y}).show();
   $("#contextmenu #"+contextMenuId+' input[type="text"]').focus();
-  
+
   suggestLinksInContextMenu();
 }
 
@@ -651,7 +652,7 @@ function suggestLinksInContextMenu() {
 
   if ($('#contextmenu #addLinkContextMenu input[name="search"]').val().length == 0)
     return;
-    
+
   var searchesReturned = 0;
   var resultsReturned = 0;
 
